014594e7922978fa2a7517479311812716ace3ba
vaibhavpatil2734 <vaibhavpatil.2734@gmail.com>
2024-07-15 21:31:40 +0530

    MyCreationUpdate2

--- src/Components/MyCreation/MyCreation.js
+++ src/Components/MyCreation/MyCreation.js
@@ -3,6 +3,19 @@ import "./MyCreation.css"
 
 export default function MyCreation() {
   return (
-    <div className='i-creation'><h1>MyCreation</h1></div>
+    <div className='i-creation'><h1>MyCreation</h1>
+   
+<div class="card animated slideInFromLeft">
+    <div class="card-body">
+        <h5 class="card-title">Card Title</h5>
+        <p class="card-text">This is a sample card with an entering effect from the left side.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    </div>
   )
 }
