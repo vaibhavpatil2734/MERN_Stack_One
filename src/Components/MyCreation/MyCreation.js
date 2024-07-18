73ed57ff48a5bd9306a255969d3ecc487859274e
vaibhavpatil2734 <vaibhavpatil.2734@gmail.com>
2024-07-18 21:29:19 +0530

    update

--- src/Components/MyCreation/MyCreation.js
+++ src/Components/MyCreation/MyCreation.js
@@ -4,7 +4,9 @@ import "./MyCreation.css"
 export default function MyCreation() {
   return (
     <div className='i-creation'><h1>MyCreation</h1>
-   
+
+    <img className="profileIMG" src="https://imgs.search.brave.com/3CvjgpvAU9R_MdSKYOKfHPmazns-d8E6sDEJH-vx0UA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNi8x/MS8yOS8xMy8yNS9h/Y2UtMTg2OTgyNV82/NDAuanBn" alt="Cinque Terre" width="400" height="400"/>
+
     </div>
   )
 }
