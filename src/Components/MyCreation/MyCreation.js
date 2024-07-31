413d1e15588fbc8d19fc3434ecb43e5d9311c8c6
vaibhavpatil2734 <vaibhavpatil.2734@gmail.com>
2024-07-31 17:31:37 +0530

    mycreation_update

--- src/Components/MyCreation/MyCreation.js
+++ src/Components/MyCreation/MyCreation.js
@@ -1,12 +1,41 @@
-import React from 'react'
-import "./MyCreation.css"
+import React from 'react';
+import { motion } from 'framer-motion';
+import './MyCreation.css';
 
 export default function MyCreation() {
   return (
-    <div className='i-creation'>
-    <div>
-      <img className="profileIMG m-3" src="https://imgs.search.brave.com/3CvjgpvAU9R_MdSKYOKfHPmazns-d8E6sDEJH-vx0UA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNi8x/MS8yOS8xMy8yNS9h/Y2UtMTg2OTgyNV82/NDAuanBn" alt="Cinque Terre" width="200" height="200"/>
-    </div>
-    </div>
-  )
+    <motion.div 
+      className='i-creation'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 2 }}
+    >
+      <motion.div 
+        className="image-container"
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ duration: 1, delay: 0.5 }}
+      >
+        <motion.img 
+          className="profileIMG" 
+          src="https://imgs.search.brave.com/fk8PPoN9nmFBJqocbJ_6D4-iFMK6F7rpWbY9lOLYdJk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS12ZWN0/b3Iva2l0dGVuLWNh/dC1pbWdlXzExMzg1/NDQtMjgxNTYuanBn/P3NpemU9NjI2JmV4/dD1qcGc" 
+          alt="Cinque Terre" 
+          width="200" 
+          height="200"
+          whileHover={{ scale: 1.2, rotate: 360 }}
+          transition={{ duration: 0.5 }}
+        />
+      </motion.div>
+      <motion.div 
+        className="text-line"
+        initial={{ x: -200, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1, delay: 1.5 }}
+      >
+        <h1 className='text-light'>
+          <b>Welcome to My Creation!</b>
+        </h1>
+      </motion.div>
+    </motion.div>
+  );
 }
